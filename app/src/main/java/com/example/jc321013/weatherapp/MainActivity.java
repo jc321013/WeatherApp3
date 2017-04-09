9c3ea52e2082d41ca221eb9e51fc1eea7ea18be8
jc321013 <jared.marcolongo.jcu.edu.au>
2017-04-09 13:59:36 +1000

    jn

--- app/src/main/java/com/example/jc321013/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/jc321013/weatherapp/MainActivity.java
@@ -1,13 +1,97 @@
 package com.example.jc321013.weatherapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.jc321013.weatherapp.Model.OpenWeatherMap;
+
+import org.w3c.dom.Text;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.INTERNET;
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    TextView txtCity, txtLastUpdate, txtDescription, txtHumidity, txtTime, txtCelsius;
+    ImageView imageView;
+
+    LocationManager locationManager;
+    String provider;
+    static double lat, lng;
+    OpenWeatherMap openWeatherMap = new OpenWeatherMap();
+
+    int MY_PERMISSION = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Control
+        txtCity = (TextView) findViewById(R.id.txtCity);
+        txtLastUpdate = (TextView) findViewById(R.id.txtLastUpdate);
+        txtDescription = (TextView) findViewById(R.id.txtDescription);
+        txtHumidity = (TextView) findViewById(R.id.txtHumidity);
+        txtTime = (TextView) findViewById(R.id.txtTime);
+        txtCelsius = (TextView) findViewById(R.id.txtCelsius);
+
+
+        //Get Coordinates
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), false);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+           ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                   Manifest.permission.INTERNET,
+                   Manifest.permission.ACCESS_COARSE_LOCATION,
+                   Manifest.permission.ACCESS_FINE_LOCATION,
+                   Manifest.permission.ACCESS_NETWORK_STATE,
+                   Manifest.permission.SYSTEM_ALERT_WINDOW,
+                   Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+
+
+
+
+                   }, MY_PERMISSION);
+
+
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+        if(location == null)
+            LOG.e("TAG", "No Location");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
